3d21b227063387e859788d0cdc8f71a760132a84
Elie Zananiri <prisonerjohn@gmail.com>
2015-03-17 10:12:46 -0400

    Added Visual Studio 2012 project file. Updated gitignore.

--- example_ofxMarchingCubes/addons.make
+++ example_ofxMarchingCubes/addons.make
@@ -1,2 +1,1 @@
-addons
 ofxMarchingCubes
--- example_ofxMarchingCubes/src/main.cpp
+++ example_ofxMarchingCubes/src/main.cpp
@@ -1,15 +1,10 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
 //========================================================================
-int main( ){
+int main(){
 
-    ofAppGlutWindow window;
-	
-	window.setGlutDisplayString("rgba double samples>=4 depth");
-	
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+    ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
